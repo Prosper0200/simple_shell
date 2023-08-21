8d816b5030862b63c16ccdbe8045aa3edbf08fa3
Prosper0200 <agarreyiruoghene@gmail.com>
2023-08-21 07:07:53 -0700

    simple_shell

--- simp1.c
+++ simp1.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-int main()
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+int main(void)
 {
+pid_t child_pid;
+int status;
 char *line = NULL;
 size_t len = 0;
 ssize_t read;
@@ -12,11 +16,31 @@ printf("$ ");
 read = getline(&line, &len, stdin);
 if (read != -1)
 {
-printf("%s", line);
+child_pid = fork();
+if (child_pid == -1)
+{
+perror("Error:");
+printf("\n");
+exit(1);
+}
+if (child_pid == 0)
+{
+char *arg[] = {line, NULL};
+if (execve("/bin/sh", arg, NULL) == -1)
+{
+perror("Error");
+}
+exit(0);
+}
+else
+{
+wait(&status);
+}
 }
 else if (read == -1)
 {
 break;
 }
 }
+return (0);
 }
